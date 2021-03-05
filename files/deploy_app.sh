2b42c9a36b7636fc5debbc0f9e395f81b96f3147
kartheeka-SRE <kartheeka@gmail.com>
2021-03-05 15:42:41 -0500

    Update deploy_app.sh
    added some big text as part of training excercise to see if these changes are reflected

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -9,6 +9,23 @@ cat << EOM > /var/www/html/index.html
   <div style="width:800px;margin: 0 auto">
 
   <!-- BEGIN -->
+  index=uwcrdtsvc splunk_server_group=TAWS logGroup="/aws/ecs/uwcrdtsvc-ecs-acpt/uwcrdtsvc-credit-retrieval-service"  ALL_CRAS_DURATION_MS
+    "cwmessage.springAppName"="creditRetrieval" | search cwmessage.x-fnma-correlation-id=UWT3DEA2EEBE38A47118396FF4152306141  
+    | stats values(cwmessage.ALL_CRAS_DURATION_MS) AS CRA
+      
+| appendcols [search index=uwloanapp splunk_server_group=TAWS logGroup="/aws/ecs/uwloanapp-ecs-acpt/uwloanapp-uw-entry-service-service" 
+("cwmessage.SERVICE_STATUS"="SUCCESS") "cwmessage.MSG_TYPE"="SERVICE_RESPONSE" "cwmessage.REQUEST_TYPE"="GetCreditReport" 
+"cwmessage.HTTP_CODE"="200" "cwmessage.springAppName"="UW-entry-service" | search cwmessage.x_fnma_correlation_id=UWT3DEA2EEBE38A47118396FF4152306141 OR cwmessage.x-fnma-correlation-id=UWT3DEA2EEBE38A47118396FF4152306141   
+| stats values(cwmessage.SERVICE_DURATION_MS) as E2E_RoundTrip ]
+
+| appendcols [search index=uwcrdtsvc splunk_server_group=TAWS logGroup="/aws/ecs/uwcrdtsvc-ecs-acpt/uwcrdtsvc-credit-retrieval-service" 
+("cwmessage.SERVICE_STATUS"="SUCCESS") "cwmessage.MSG_TYPE"="SERVICE_RESPONSE"  "cwmessage.springAppName"="creditRetrieval"  "cwmessage.RESPONSE_TYPE"=XIS_CREDIT_ONLY | search cwmessage.x-fnma-correlation-id=UWT3DEA2EEBE38A47118396FF4152306141  
+| stats values(cwmessage.SERVICE_DURATION_MS) AS CrdRtrvl_RT ] 
+
+| eval CrdRtrvl  = CrdRtrvl_RT - CRA
+| eval EntryService  = E2E_RoundTrip - CrdRtrvl_RT
+| table CRA, CrdRtrvl, EntryService, E2E_RoundTrip
+
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
   Welcome to ${PREFIX}'s app. Replace this text with your own.
